bad1193e9db8cc2e74e8b26db09dd1ed6e2badbf
joudikab01 <joudi.kab01@gmail.com>
2022-06-23 15:15:38 +0300

    add teachers without salary and subjects

--- lib/screens/teachers/add.dart
+++ lib/screens/teachers/add.dart
@@ -10,14 +10,38 @@ class AddTeacher extends StatefulWidget {
 }
 
 class _AddTeacherState extends State<AddTeacher> {
+  final fnameController = TextEditingController();
+  final lnameController = TextEditingController();
+  final emailController = TextEditingController();
+
+  final FocusNode focusNode1 = FocusNode();
+  final FocusNode focusNode2 = FocusNode();
+  final FocusNode focusNode3 = FocusNode();
+
+  String genderDDV = 'male';
+  String bloodDDV = 'male';
+
+  @override
+  initState() {
+    focusNode1.addListener(() {
+      setState(() {});
+    });
+    focusNode2.addListener(() {
+      setState(() {});
+    });
+    focusNode3.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
 
   DateTime? _selectedDate;
   void _presentDatePicker() {
     showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime(2020),
-        lastDate: DateTime.now())
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2020),
+            lastDate: DateTime.now())
         .then((pickedDate) {
       if (pickedDate == null) {
         return;
@@ -30,56 +54,261 @@ class _AddTeacherState extends State<AddTeacher> {
 
   @override
   Widget build(BuildContext context) {
-    // return Container(color: Colors.red,);
     return ListView(
-        children: [
-          //fname lname email
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(width:widgetSize.getWidth(50, context),child: TextFormField()),
-                Container(width:widgetSize.getWidth(50, context),child: TextFormField()),
-                Container(width:widgetSize.getWidth(50, context),child: TextFormField()),
-              ],
+      padding: const EdgeInsets.all(
+        30,
+      ),
+      children: [
+        const Center(
+          child: Text(
+            'Add Teacher',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
           ),
-          //gender  bloodtype address grades
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                DropdownButton<String?>(
-                  items: [],
-                  onChanged: (value) {},
-                ),
-                DropdownButton<String?>(
-                  items: [],
-                  onChanged: (value) {},
-                ),
-                DropdownButton<String?>(
-                  items: [],
-                  onChanged: (value) {},
-                ),
-                DropdownButton<String?>(
-                  items: [],
-                  onChanged: (value) {},
-                ),
-              ],
-            ),
+        ),
+        //fname lname email
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 30,
           ),
-          //join date
-          ElevatedButton(
-            onPressed: () {
-              _presentDatePicker();
-            },
-            child: Text(
-              'date',
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: widgetSize.getWidth(
+                  70,
+                  context,
+                ),
+                child: TextFormField(
+                  focusNode: focusNode1,
+                  cursorColor: const Color(
+                    0Xff2BC3BB,
+                  ),
+                  decoration: InputDecoration(
+                    hoverColor: const Color(
+                      0Xff2BC3BB,
+                    ),
+                    focusColor: const Color(
+                      0Xff2BC3BB,
+                    ),
+                    labelStyle: TextStyle(
+                      color: focusNode1.hasFocus
+                          ? const Color(
+                        0Xff2BC3BB,
+                      )
+                          : Colors.black54,
+                    ),
+                    labelText: "First name",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      borderSide: const BorderSide(
+                        color: Color(0Xff2BC3BB),
+                        width: 2,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      borderSide: const BorderSide(
+                        color: Color(0Xff2BC3BB),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  controller: fnameController,
+                ),
+              ),
+              SizedBox(
+                width: widgetSize.getWidth(
+                  70,
+                  context,
+                ),
+                child: TextFormField(
+                  focusNode: focusNode2,
+                  cursorColor: const Color(
+                    0Xff2BC3BB,
+                  ),
+                  decoration: InputDecoration(
+                    hoverColor: const Color(
+                      0Xff2BC3BB,
+                    ),
+                    focusColor: const Color(
+                      0Xff2BC3BB,
+                    ),
+                    labelStyle: TextStyle(
+                      color: focusNode2.hasFocus
+                          ? const Color(
+                        0Xff2BC3BB,
+                      )
+                          : Colors.black54,
+                    ),
+                    labelText: "Last name",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      borderSide: const BorderSide(
+                        color: Color(0Xff2BC3BB),
+                        width: 2,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      borderSide: const BorderSide(
+                        color: Color(0Xff2BC3BB),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  controller: lnameController,
+                ),
+              ),
+              SizedBox(
+                width: widgetSize.getWidth(
+                  70,
+                  context,
+                ),
+                child: TextFormField(
+                  focusNode: focusNode3,
+                  cursorColor: const Color(
+                    0Xff2BC3BB,
+                  ),
+                  decoration: InputDecoration(
+                    hoverColor: const Color(
+                      0Xff2BC3BB,
+                    ),
+                    focusColor: const Color(
+                      0Xff2BC3BB,
+                    ),
+                    labelStyle: TextStyle(
+                      color: focusNode3.hasFocus
+                          ? const Color(
+                        0Xff2BC3BB,
+                      )
+                          : Colors.black54,
+                    ),
+                    labelText: "Email address",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      borderSide: const BorderSide(
+                        color: Color(0Xff2BC3BB),
+                        width: 2,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      borderSide: const BorderSide(
+                        color: Color(0Xff2BC3BB),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  controller: emailController,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
+        //gender blood joining date
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 30.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              DropdownButton<String>(
+                hint: Text(
+                  'Gender',
+                ),
+                value: genderDDV,
+                elevation: 16,
+                underline: Container(
+                  height: 2,
+                  color: const Color(
+                    0Xff2BC3BB,
+                  ),
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    genderDDV = newValue!;
+                  });
+                },
+                items: <String>['male', 'female']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              DropdownButton<String>(
+                hint: Text(
+                  'Blood type',
+                ),
+                value: bloodDDV,
+                elevation: 16,
+                underline: Container(
+                  height: 2,
+                  color: const Color(
+                    0Xff2BC3BB,
+                  ),
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    bloodDDV = newValue!;
+                  });
+                },
+                items: <String>['male', 'female']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              Container(
+                width: widgetSize.getWidth(60, context),
+                height: widgetSize.getHeight(100, context),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(
+                      0Xff2BC3BB,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        15,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    _presentDatePicker();
+                  },
+                  child: const Text(
+                    'Joining date',
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+      ],
     );
   }
 }
